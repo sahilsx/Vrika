32c679134805c489b875a9fb4866994408623efb
sahil <itxsaaho@gmail.com>
2024-08-30 13:21:33 +0530

    responsive commit

--- Vrika/obj/Debug/net8.0/Vrika.AssemblyInfo.cs
+++ Vrika/obj/Debug/net8.0/Vrika.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Vrika")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66e994a1ac24921973ae6d3aa89c0d1f25ef69f2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2a7639066573350232775f1c9950e337bc793d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Vrika")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Vrika")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
